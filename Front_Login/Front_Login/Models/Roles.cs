1c143c43cee46fcc1995e9e0f9bf819e115037d0
davidnavarrete <davidnava1998efrain@outlook.com>
2025-09-22 11:26:12 -0500

    semana 11
    vista login

---
+++ Front_Login/Front_Login/Models/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Front_Login.Models
+{
+	public class Roles
+	{
+        [JsonProperty("idRol")]
+        public int IdRol { get; set; }
+        [JsonProperty("nombreRol")]
+        public string NombreRol { get; set; }
+        [JsonProperty("estadoRol")]
+        public string EstadoRol { get; set; }
+	}
+}
+
---
+++ Front_Login/Front_Login/Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace Front_Login.Models
+{
+    public class Usuario
+    {
+        [JsonProperty("idUsuario")]
+        public int IdUsuario { get; set; }
+        [JsonProperty("nombreUsuario")]
+        public string NombreUsuario { get; set; }
+        [JsonProperty("apellidoUsuario")]
+        public string ApellidosUsuario { get; set; }
+        [JsonProperty("emailUsuario")]
+        public string EmailUsuario { get; set; }
+        [JsonProperty("userUsuario")]
+        public string UserUsuario { get; set; }
+        [JsonProperty("passUsuario")]
+        public string PassUsuario { get; set; }
+        [JsonProperty("estadoUsuario")]
+        public string Estado { get; set; }
+        [JsonProperty("fkIdRol")]
+        public Roles FkIdRol { get; set; }
+
+    }
+}
+
