e4bc576fe57554778c424f74c58bdd76c0b6bdea
davidnavarrete <davidnava1998efrain@outlook.com>
2025-09-22 11:53:08 -0500

    metodo login controller

--- Front_Login/Front_Login/Controllers/LoginController.cs
+++ Front_Login/Front_Login/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Front_Login.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +13,34 @@ namespace Front_Login.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly HttpClient _httpClient;
+
+        public LoginController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            var jsonContent = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+
+            return View("Index");
+
+        }
+       
     }
 }
 
---
+++ Front_Login/Front_Login/Models/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Front_Login.Models
+{
+	public class LoginRequest
+	{
+		[JsonProperty("userUsuario")]
+		public string UserUsUario { get; set; }
+        [JsonProperty("passUsuario")]
+        public String PassUsuario { get; set; }
+		
+	}
+}
+
